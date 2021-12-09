934b589a136955caa482815e7c3637ee00013fc5
WarZone762 <uzi1m1@gmail.com>
2021-12-10 02:24:52 +0300

    Added plugin: gitsigns.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,17 +1,21 @@
 return require("packer").startup(function()
+	-- Packer
 	use "wbthomason/packer.nvim"
 
-	use "tpope/vim-fugitive"
-	use "tpope/vim-surround"
+	-- Disabled
 	use {"scrooloose/nerdtree", disable = true}
 	use {"scrooloose/syntastic", disable = true}
 	use {"airblade/vim-gitgutter", disable = true}
 	use {"vim-airline/vim-airline", disable = true}
+	use {"w0rp/ale", disable = true}
+	use {"valloric/youcompleteme", run = "install.py --msvc 17 --cmake-path \"C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe\"", disable = true}
+
+	-- Vim
+	use "tpope/vim-fugitive"
+	use "tpope/vim-surround"
 	use "scrooloose/nerdcommenter"
 	use "majutsushi/tagbar"
-	-- use "valloric/youcompleteme', {'do': 'install.py --msvc 17 --cmake-path \"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe\"'"
 	use "tpope/vim-repeat"
-	use {"w0rp/ale", disable = true}
 	
 	use "yggdroot/indentline"
 	use "easymotion/vim-easymotion"
@@ -34,12 +38,14 @@ return require("packer").startup(function()
 	use "tpope/vim-vinegar"
 	use "tpope/vim-sensible"
 		
+	-- Neovim
 	use {"ms-jpq/coq_nvim", branch = "coq", requires = {"ms-jpq/coq.artifacts", branch = "artifacts"}, run = ":COQdeps", config = vim.api.nvim_command("autocmd VimEnter * COQnow")}
 	use {"nvim-telescope/telescope.nvim", requires = {"nvim-lua/plenary.nvim"}}
 	use {"liuchengxu/vista.vim"}
 	use {"ms-jpq/chadtree", branch = "chad", run = {"python -m chadtree deps", ":CHADdeps"}}
 	use {"nvim-lualine/lualine.nvim", config = require("lualine").setup()}
 	use {"TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim"}
+	use {"lewis6991/gitsigns.nvim", requires = "nvim-lua/plenary.nvim", config = require("gitsigns").setup()}
 
 end)
 
