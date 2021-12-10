e0aa510ba5a5657580628c92e483713ad964e4a5
WarZone762 <uzi1m1@gmail.com>
2021-12-11 01:22:00 +0300

    Refactoring, replaced CHADTree with nvim-tree, disabled Neogit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,42 +1,57 @@
 return require("packer").startup(function()
 	-- Packer
-	use "wbthomason/packer.nvim"
+	use {"wbthomason/packer.nvim"}
 
 	-- Disabled
-	use {"scrooloose/nerdtree", disable = true}
-	use {"scrooloose/syntastic", disable = true}
-	use {"airblade/vim-gitgutter", disable = true}
-	use {"vim-airline/vim-airline", disable = true}
-	use {"w0rp/ale", disable = true}
-	use {"valloric/youcompleteme", run = "install.py --msvc 17 --cmake-path \"C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe\"", disable = true}
-	use {"yggdroot/indentline", disable = true}
+	-- use {"scrooloose/nerdtree"}
+	-- use {"scrooloose/syntastic"}
+	-- use {"airblade/vim-gitgutter"}
+	-- use {"vim-airline/vim-airline"}
+	-- use {"w0rp/ale"}
+	-- use {
+	-- 	"valloric/youcompleteme",
+	-- 	run = "install.py --msvc 17 --cmake-path \"C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe\""
+	-- }
+	-- use {"yggdroot/indentline"}
+	-- use {
+	-- 	"ms-jpq/chadtree",
+	-- 	branch = "chad",
+	-- 	run = {
+	-- 		"python3 -m chadtree deps",
+	-- 		":CHADdeps"
+	-- 	}
+	-- }
+	-- use {
+	-- 	"TimUntersberger/neogit",
+	-- 	requires = {"nvim-lua/plenary.nvim"}
+	-- }
 
 	-- Vim
-	use "tpope/vim-fugitive"
-	use "tpope/vim-surround"
-	use "scrooloose/nerdcommenter"
-	use "majutsushi/tagbar"
-	use "tpope/vim-repeat"
+	use {"tpope/vim-fugitive"}
+	use {"tpope/vim-surround"}
+	use {"scrooloose/nerdcommenter"}
+	use {"majutsushi/tagbar"}
+	use {"tpope/vim-repeat"}
 	
-	use "easymotion/vim-easymotion"
-	use "mg979/vim-visual-multi"
-	use "luochen1990/rainbow"
-	use "tpope/vim-eunuch"
-	use "editorconfig/editorconfig-vim"
-	use "tpope/vim-abolish"
-	use "raimondi/delimitmate"
-	use "kkoomen/vim-doge"
-	use "terryma/vim-expand-region"
-	use "sjl/gundo.vim"
-	use "godlygeek/tabular"
-	use "wellle/targets.vim"
-	use "kana/vim-textobj-user"
-	use "kana/vim-textobj-line"
-	use "michaeljsmith/vim-indent-object"
-	use "kana/vim-textobj-entire"
-	use "tpope/vim-unimpaired"
-	use "tpope/vim-vinegar"
-	use "tpope/vim-sensible"
+	use {"easymotion/vim-easymotion"}
+	use {"mg979/vim-visual-multi"}
+	use {"luochen1990/rainbow"}
+	use {"tpope/vim-eunuch"}
+	use {"editorconfig/editorconfig-vim"}
+	use {"tpope/vim-abolish"}
+	use {"raimondi/delimitmate"}
+	use {"kkoomen/vim-doge"}
+	use {"terryma/vim-expand-region"}
+	use {"sjl/gundo.vim"}
+	use {"godlygeek/tabular"}
+	use {"wellle/targets.vim"}
+	use {"kana/vim-textobj-user"}
+	use {"kana/vim-textobj-line"}
+	use {"michaeljsmith/vim-indent-object"}
+	use {"kana/vim-textobj-entire"}
+	use {"tpope/vim-unimpaired"}
+	use {"tpope/vim-vinegar"}
+	use {"tpope/vim-sensible"}
 		
 	-- Neovim
 	use {
@@ -57,20 +72,13 @@ return require("packer").startup(function()
 		"liuchengxu/vista.vim"
 	}
 	use {
-		"ms-jpq/chadtree",
-		branch = "chad",
-		run = {
-			"python3 -m chadtree deps",
-			":CHADdeps"
-		}
+		"kyazdani42/nvim-tree.lua",
+		requires = {"kyazdani42/nvim-web-devicons"},
+		config = function() require("nvim-tree").setup() end
 	}
 	use {
 		"nvim-lualine/lualine.nvim",
 		config = function() require("lualine").setup() end
-	}
-	use {
-		"TimUntersberger/neogit",
-		requires = {"nvim-lua/plenary.nvim"}
 	}
 	use {
 		"lewis6991/gitsigns.nvim",
