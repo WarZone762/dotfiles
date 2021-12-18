952cf46f0549fb2042dc105a0b75458c249453ac
WarZone762 <uzi1m1@gmail.com>
2021-12-18 22:16:03 +0300

    Changed temporary folder to system's

--- init.lua
+++ init.lua
@@ -5,9 +5,9 @@ set.relativenumber = true
 -- set.t_Co = 256
 set.termguicolors = true
 
-set.directory = "F:/Temp/Neovim//"
-set.backupdir = "F:/Temp/Neovim//"
-set.undodir = "F:/Temp/Neovim//"
+set.directory = vim.env.TEMP .. "/nvim//"
+set.backupdir = vim.env.TEMP .. "/nvim//"
+set.undodir = vim.env.TEMP .. "/nvim//"
 
 if string.find(vim.env.ComSpec, "cmd.exe") then
 	set.shell = "powershell"
