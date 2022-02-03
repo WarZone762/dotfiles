6989c2a5670d27d16f6170d63bf69962b5249203
WarZone762 <uzi1m1@gmail.com>
2022-02-03 13:20:00 +0300

    Fixed "RRethy/vim-illuminate" initialization and "windwp/nvim-autopairs" <BS> and <CR> mapping

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -27,31 +27,8 @@ return require("packer").startup(function(use)
 	use {"tpope/vim-sensible"}
 
 	-- Neovim
+  use {"RRethy/vim-illuminate"}
   use {"L3MON4D3/LuaSnip"}
-	use {"neovim/nvim-lspconfig"}
-  use {
-    "williamboman/nvim-lsp-installer",
-    config = function() require("nvim-lsp-installer").on_server_ready(
-      function(server)
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities.textDocument.completion.completionItem.snippetSupport =
-          true
-
-        local opts = {
-          capabilities = capabilities,
-        }
-        server:setup(opts)
-      end
-    ) end
-  }
-	use {
-		"nvim-treesitter/nvim-treesitter",
-		config = function() require("nvim-treesitter.configs").setup({
-			ensure_installed = "maintained",
-			highlight = {enable = true}
-		}) end,
-		run = ":TSUpdate"
-	}
   use {
     "ms-jpq/coq_nvim",
     branch = "coq",
@@ -71,8 +48,45 @@ return require("packer").startup(function(use)
       }
     },
     run = ":COQdeps",
-    config = "vim.g.coq_settings = {auto_start = 'shut-up'}"
+    config = function() vim.g.coq_settings = {auto_start = 'shut-up'} end
   }
+	use {"neovim/nvim-lspconfig"}
+  use {
+    "williamboman/nvim-lsp-installer",
+    after = {"vim-illuminate", "coq_nvim"},
+    config = function() require("nvim-lsp-installer").on_server_ready(
+      function(server)
+        local function on_attach(client, bufnr)
+          require("illuminate").on_attach(client)
+        end
+
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.completion.completionItem.snippetSupport =
+          true
+
+        local coq = require("coq")
+
+        local opts = {
+          on_attach = on_attach,
+          capabilities = capabilities,
+        }
+
+        for key, value in pairs(coq.lsp_ensure_capabilities()) do
+          opts[key] = value
+        end
+
+        server:setup(opts)
+      end
+    ) end
+  }
+	use {
+		"nvim-treesitter/nvim-treesitter",
+		config = function() require("nvim-treesitter.configs").setup({
+			ensure_installed = "maintained",
+			highlight = {enable = true}
+		}) end,
+		run = ":TSUpdate"
+	}
 	use {"mfussenegger/nvim-dap"}
 	use {
 		"nvim-telescope/telescope.nvim",
@@ -129,12 +143,6 @@ return require("packer").startup(function(use)
     config = function() require("lspsaga").setup() end
   }
   use {
-    "RRethy/vim-illuminate",
-    config = function()
-      vim.defer_fn(function() require("illuminate").on_attach() end, 5000)
-    end
-  }
-  use {
     "akinsho/bufferline.nvim",
     requires = "kyazdani42/nvim-web-devicons",
     config = require("bufferline").setup()
@@ -145,24 +153,25 @@ return require("packer").startup(function(use)
     config = function() vim.defer_fn(function()
       local autopairs = require("nvim-autopairs")
 
-      autopairs.setup({map_bs = true, map_cr = false, check_ts = true})
+      autopairs.setup({map_bs = false, map_cr = false, check_ts = true})
 
       _G.AutopairsFunctions = {}
 
       AutopairsFunctions.CR = function()
         if vim.fn.pumvisible() ~= 0 then
           if vim.fn.complete_info({"selected"}).selected ~= -1 then
-            return autopairs.esc("<c-y>")
+            return autopairs.esc("<C-y>")
           else
-            return autopairs.esc("<c-e>") .. autopairs.autopairs_cr()
+            return autopairs.esc("<C-e>") ..
+              autopairs.autopairs_cr(vim.api.nvim_get_current_buf())
           end
         else
-          return autopairs.autopairs_cr()
+          return autopairs.autopairs_cr(vim.api.nvim_get_current_buf())
         end
       end
       vim.api.nvim_set_keymap(
         "i",
-        "<cr>",
+        "<CR>",
         "v:lua.AutopairsFunctions.CR()",
         {expr = true, noremap = true}
       )
@@ -172,14 +181,15 @@ return require("packer").startup(function(use)
           vim.fn.pumvisible() ~= 0
           and vim.fn.complete_info({"mode"}).mode == "eval"
         then
-          return autopairs.esc("<c-e>") .. autopairs.autopairs_bs()
+          return autopairs.esc("<C-e>") ..
+            autopairs.autopairs_bs(vim.api.nvim_get_current_buf())
         else
-          return autopairs.autopairs_bs()
+          return autopairs.autopairs_bs(vim.api.nvim_get_current_buf())
         end
       end
       vim.api.nvim_set_keymap(
         "i",
-        "<bs>",
+        "<BS>",
         "v:lua.AutopairsFunctions.BS()",
         {expr = true, noremap = true}
       )
