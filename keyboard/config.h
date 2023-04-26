cc6d7f332764f7b50e6e44a51a43c22ecaf51344
WarZone762 <uzi1m1@gmail.com>
2023-04-26 18:50:29 +0300

    Added Vial config

---
+++ keyboard/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID \
+    { 0x33, 0x12, 0x25, 0x7D, 0x3D, 0x66, 0xEE, 0xD8 }
+
+// Left Shift + Left Alt
+#define VIAL_UNLOCK_COMBO_ROWS \
+    { 5, 5 }
+#define VIAL_UNLOCK_COMBO_COLS \
+    { 5, 6 }
