bf82c25d6f85417f2157288d5605c0d0e710784e
WarZone762 <uzi1m1@gmail.com>
2023-05-01 12:39:31 +0300

    Switched to DVORAK and vanilla QMK

---
+++ keyboard/macros.c
@@ -0,0 +1,38 @@
+#include QMK_KEYBOARD_H
+
+enum custom_keycodes {
+    REPEAT_M1 = SAFE_RANGE,
+    REPEAT_M2,
+};
+
+bool repeat_m1 = false;
+bool repeat_m2 = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case REPEAT_M1:
+            if (record->event.pressed) {
+                repeat_m1 = true;
+            } else {
+                repeat_m1 = false;
+            }
+            break;
+        case REPEAT_M2:
+            if (record->event.pressed) {
+                repeat_m2 = true;
+            } else {
+                repeat_m2 = false;
+            }
+            break;
+    }
+    return true;
+};
+
+void matrix_scan_user(void) {
+    if (repeat_m1) {
+        tap_code(KC_MS_BTN1);
+    }
+    if (repeat_m2) {
+        tap_code(KC_MS_BTN2);
+    }
+}
