b5d940564d590236a30f1532981e1618a381d1a7
WarZone762 <uzi1m1@gmail.com>
2023-09-19 20:18:59 +0300

    Changed terminal background

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,11 +11,7 @@ config.audible_bell = "Disabled"
 config.font = wezterm.font "CaskaydiaCove Nerd Font"
 config.color_scheme = "rose-pine"
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_image = os.getenv("DOTFILES_DIR") .. "/milky-way.jpg"
-config.window_background_image_hsb = {
-    brightness = 0.1,
-    hue = 1.0,
-    saturation = 1.0,
-}
+config.window_background_image = os.getenv("DOTFILES_DIR") .. "/rcw-106.jpg"
+config.window_background_opacity = 0.125
 
 return config
