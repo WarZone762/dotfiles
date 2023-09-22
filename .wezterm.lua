bede8b043ef2101b02f9406ad2860fb5244c2fd6
WarZone762 <uzi1m1@gmail.com>
2023-09-22 19:13:02 +0300

    change(wezterm): changed background

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,7 +11,28 @@ config.audible_bell = "Disabled"
 config.font = wezterm.font "CaskaydiaCove Nerd Font"
 config.color_scheme = "rose-pine"
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_image = os.getenv("DOTFILES") .. "/rcw-106.jpg"
-config.window_background_opacity = 0.125
+config.background = {
+    {
+        source = {
+            File = os.getenv("DOTFILES") .. "/rcw-106.jpg",
+        },
+        opacity = 0.125,
+    },
+    {
+        source = {
+            File = os.getenv("DOTFILES") .. "/posture-ironic.jpg",
+        },
+        height = "750",
+        width = "750",
+        hsb = {
+            brightness = 0.02,
+        },
+        horizontal_align = "Right",
+        vertical_align = "Middle",
+        horizontal_offset = "-50px",
+        repeat_x = "NoRepeat",
+        repeat_y = "NoRepeat",
+    },
+}
 
 return config
