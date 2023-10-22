111dd84935b643354a30d3c66c57007ddd7f904c
WarZone762 <uzi1m1@gmail.com>
2023-10-22 04:25:38 +0300

    fix(keyboard): M1 & M2 macros firing too fast (added 30 ms delay)

--- keyboard/src/macros.c
+++ keyboard/src/macros.c
@@ -1,38 +1,39 @@
 #include QMK_KEYBOARD_H
 
+#define REPEAT_DELAY 30
+
 enum {
     REPEAT_M1 = SAFE_RANGE,
     REPEAT_M2,
 };
 
 bool repeat_m1 = false;
+uint16_t repeat_timer_m1 = 0;
+
 bool repeat_m2 = false;
+uint16_t repeat_timer_m2 = 0;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case REPEAT_M1:
-            if (record->event.pressed) {
-                repeat_m1 = true;
-            } else {
-                repeat_m1 = false;
-            }
+            repeat_m1 = record->event.pressed;
+            repeat_timer_m1 = timer_read();
             break;
         case REPEAT_M2:
-            if (record->event.pressed) {
-                repeat_m2 = true;
-            } else {
-                repeat_m2 = false;
-            }
+            repeat_m2 = record->event.pressed;
+            repeat_timer_m2 = timer_read();
             break;
     }
     return true;
 };
 
 void matrix_scan_user(void) {
-    if (repeat_m1) {
+    if (repeat_m1 && timer_elapsed(repeat_timer_m1) > REPEAT_DELAY) {
         tap_code(KC_MS_BTN1);
+        repeat_timer_m1 += REPEAT_DELAY;
     }
-    if (repeat_m2) {
+    if (repeat_m2 && timer_elapsed(repeat_timer_m2) > REPEAT_DELAY) {
         tap_code(KC_MS_BTN2);
+        repeat_timer_m2 += REPEAT_DELAY;
     }
 }
