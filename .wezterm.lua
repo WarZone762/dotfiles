8791516460b46e559e7dcf568b450266480833a9
WarZone762 <uzi1m1@gmail.com>
2023-12-20 08:21:18 +0300

    feat(wezterm): added custom colorscheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,7 +10,45 @@ config.default_prog = { "tmux-with-config" }
 config.term = "wezterm"
 config.audible_bell = "Disabled"
 config.font = wezterm.font "CaskaydiaCove Nerd Font"
-config.color_scheme = "rose-pine"
+-- config.color_scheme = "rose-pine"
+local bg = "#0D0800"
+local fg = "#FFA6A6"
+config.colors = {
+    background = bg,
+    foreground = fg,
+
+    cursor_bg = "#FFC3A6",
+    cursor_fg = "#0F0D00",
+    cursor_border = "#FFCBB3",
+
+    selection_fg = bg,
+    selection_bg = fg,
+
+    scrollbar_thumb = "#331F00",
+    split = "#663D00",
+
+    ansi = {
+        "#1A0F00",
+        "#993D3D",
+        "#3D993D",
+        "#99993D",
+        "#3D3D99",
+        "#993D99",
+        "#3D9999",
+        "#B36F5F",
+    },
+
+    brights = {
+        "#66381C",
+        "#CC2929",
+        "#29CC29",
+        "#CCCC29",
+        "#2929CC",
+        "#CC29CC",
+        "#29CCCC",
+        "#FFCCCC",
+    },
+}
 config.hide_tab_bar_if_only_one_tab = true
 config.background = {
     {
